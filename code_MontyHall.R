7b2acf0c8a332cd5849a2a349be7f3938cba8a3f
Ossifragus <whygps@gmail.com>
2025-03-30 03:40:56 +0000

    Deploying to gh-pages from @ Ossifragus/MontyHall@18fd51271a1f4032cbcbdc3870d66db5d5889055 🚀

---
+++ code_MontyHall.R
@@ -0,0 +1,22 @@
+mont <- function(choice=1) {
+  doors <- 1:3
+  car <- sample(doors, 1)
+  if (choice == car) {
+    host <- sample(doors[-c(choice)], 1)
+  } else {
+    host <- doors[-c(choice, car)]
+  }
+  switch <- doors[-c(choice, host)]
+  return(c(choice, car, switch, host))
+}
+
+countMTH <- function(n) {
+  result <- replicate(n, mont())
+  open3 <- result[4,] == 3
+  open3games <- result[, open3]
+  return(c(mean(open3games[1,] == open3games[2,]),
+           mean(open3games[3,] == open3games[2,])))
+}
+
+set.seed(1)
+system.time(countMTH(10^6))
